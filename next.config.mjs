a1a814774656ca3515ca7523397de6a6ebc251ae
fazlay <rfazlay21@gmail.com>
2024-09-09 11:40:13 +0600

    fix next config :bug:

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
-
+module.exports = nextConfig;
